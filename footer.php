8030311ce2198f46dfc7491f88c9b3f4bc7d13c9
calgaryd <calgaryd@Arushas-iMac.local>
2013-05-06 12:59:26 -0600

    responsive tweaks

--- footer.php
+++ footer.php
@@ -20,10 +20,11 @@
 				<?php dynamic_sidebar( 'footer-widget-middle' ); ?>
 			</div>
 			<div class="span4" id="footer-right">
-				<h4>Subscribe to Our Newsletter</h4>
-				<script type="text/javascript" src="http://www.formstack.com/forms/js.php?1457049-3OIpaBm6m0-v3&jsonp"></script><noscript><a href="http://www.formstack.com/forms/?1457049-3OIpaBm6m0" title="Online Form">Online Form - Constant Contact Signup Form</a></noscript>
-				<a href="http://www.formstack.com/try-formstack?utm_source=h&utm_medium=jsembed&utm_campaign=fa&fa=h,1457049" title="Web Form Generator"></a>
-				
+				<div class="widget">
+					<h4>Subscribe to Our Newsletter</h4>
+					<script type="text/javascript" src="http://www.formstack.com/forms/js.php?1457049-3OIpaBm6m0-v3&jsonp"></script><noscript><a href="http://www.formstack.com/forms/?1457049-3OIpaBm6m0" title="Online Form">Online Form - Constant Contact Signup Form</a></noscript>
+					<a href="http://www.formstack.com/try-formstack?utm_source=h&utm_medium=jsembed&utm_campaign=fa&fa=h,1457049" title="Web Form Generator"></a>
+				</div>
 			</div>
 		</div><!-- .row -->
 	</div><!-- .container -->
--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 					<h1><a href="<?php echo home_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/open-streets-calgary-logo.png" width="620" height="107" alt="Open Streets Calgary"></a></h1>
 				</hgroup>
 				
-				<nav id="site-navigation" role="navigation" class="hidden-phone">
+				<nav id="site-navigation" role="navigation">
 					<div class="screen-reader-text skip-link">
 						<a href="#content" title="<?php esc_attr_e( 'Skip to content', 'open_streets_calgary' ); ?>"><?php _e( 'Skip to content', 'open_streets_calgary' ); ?></a>
 					</div>
@@ -39,17 +39,4 @@
 					<a href="http://facebook.com/openstreetscalgary"><img src="<?php bloginfo('template_directory'); ?>/images/facebook-icon.png" width="27" height="27" alt="Open Streerts Calgary on Facebook" class="facebook-icon"></a>
 					<a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/rss-icon.png" width="27" height="27" alt="Open Streerts Calgary on Facebook" class="rss-icon"></a>
 				</nav><!-- #site-navigation -->
-				
-				<!-- Mobile Navigation -->
-				<?php 
-					if ( has_nav_menu( 'primary' ) ) {
-						wp_nav_menu( array( 
-							'show_description' => false,
-							'menu' => 'primary', 
-							'items_wrap' => '<select id="drop-nav"><option value="">Select a page...</option>%3$s</select>',
-							'container' => false,
-							'walker'  => new Walker_Nav_Menu_Dropdown(),
-							'theme_location' => 'primary')); 
-					}
-				?>				
 			</header><!-- #masthead -->
